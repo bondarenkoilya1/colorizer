c9cf5af6f0c328ed4e08bb0ce027e41351dbc9a2
Ilya <bondarenkoilya2@gmail.com>
2024-11-09 16:33:47 +0300

    add theme provider

--- client/src/components/Providers/Providers.tsx
+++ client/src/components/Providers/Providers.tsx
@@ -1,7 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { ReactNode, FC } from "react";
+import { ThemeProvider } from "@mui/material";
+import { defaultTheme } from "../../themes";
 
 // TODO: If this prop will be repeated later I can take this out to types folder
 export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 };
