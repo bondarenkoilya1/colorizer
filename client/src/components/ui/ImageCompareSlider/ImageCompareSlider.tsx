d87a014e22aedb00e6113d8c99dba76773c90e4b
Ilya <bondarenkoilya2@gmail.com>
2024-11-11 10:46:42 +0300

    remake styling of component

--- client/src/components/ui/ImageCompareSlider/ImageCompareSlider.tsx
+++ client/src/components/ui/ImageCompareSlider/ImageCompareSlider.tsx
@@ -1,11 +1,14 @@
 import { ReactCompareSlider, ReactCompareSliderImage } from "react-compare-slider";
 import { FC } from "react";
 import { ImageCompareSliderProps } from "../../../types";
+import { StyledCompareSlider } from "./styled";
+import styled from "@emotion/styled";
+
+const StyledReactCompareSlider = styled(ReactCompareSlider)(StyledCompareSlider);
 
 export const ImageCompareSlider: FC<ImageCompareSliderProps> = ({ image }) => {
   return (
-    <ReactCompareSlider
-      style={{ borderRadius: "12px", border: "3px solid #fff" }}
+    <StyledReactCompareSlider
       itemOne={<ReactCompareSliderImage src={image.darkVersion.src} alt={image.darkVersion.alt} />}
       itemTwo={
         <ReactCompareSliderImage src={image.normalVersion.src} alt={image.normalVersion.alt} />
