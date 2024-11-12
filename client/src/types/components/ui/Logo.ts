bee57eca606c8b7c53d2315a6c4ffcd4cb73befb
Ilya <bondarenkoilya2@gmail.com>
2024-11-12 13:02:48 +0300

    create logo component

---
+++ client/src/types/components/ui/Logo.ts
@@ -0,0 +1,7 @@
+/* TODO: Maybe create a special type only for color
+   TODO: Maybe set an utility type for a set of colors which will be used in all of color themes
+*/
+export type LogoProps = {
+  titleColor?: string;
+  subtitleColor?: string;
+};
