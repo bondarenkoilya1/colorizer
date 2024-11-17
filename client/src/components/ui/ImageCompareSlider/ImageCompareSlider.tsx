f596b0f5999066500cc3bb9f77bb9c6dcddeecf6
Ilya <bondarenkoilya2@gmail.com>
2024-11-17 16:27:10 +0300

    make naming changes in image compare slider component

--- client/src/components/ui/ImageCompareSlider/ImageCompareSlider.tsx
+++ client/src/components/ui/ImageCompareSlider/ImageCompareSlider.tsx
@@ -1,14 +1,14 @@
 import { ReactCompareSlider, ReactCompareSliderImage } from "react-compare-slider";
 import { FC } from "react";
 import { ImageCompareSliderProps } from "../../../types";
-import { StyledCompareSlider } from "./styled";
+import { CompareSliderStyles } from "./styled";
 import styled from "@emotion/styled";
 
-const StyledReactCompareSlider = styled(ReactCompareSlider)(StyledCompareSlider);
+const ReactCompareSliderStyled = styled(ReactCompareSlider)(CompareSliderStyles);
 
 export const ImageCompareSlider: FC<ImageCompareSliderProps> = ({ image }) => {
   return (
-    <StyledReactCompareSlider
+    <ReactCompareSliderStyled
       itemOne={<ReactCompareSliderImage src={image.darkVersion.src} alt={image.darkVersion.alt} />}
       itemTwo={
         <ReactCompareSliderImage src={image.normalVersion.src} alt={image.normalVersion.alt} />
