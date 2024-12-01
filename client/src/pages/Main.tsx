c1587d1182660378b64c5759d67874ce5c761d33
Ilya <bondarenkoilya2@gmail.com>
2024-12-01 14:39:32 +0300

    create first version of file uploader component

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,3 +1,19 @@
+// import { ImageCompareSlider } from "../components";
+// import { REPRESENTATIVE_IMAGE } from "../constants";
+import { ContainerStyled } from "../styled";
+import { FileUploader } from "../components";
+
 export const Main = () => {
-  return <>Main page</>;
+  return (
+    <ContainerStyled>
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    width: "30%",*/}
+      {/*    marginLeft: "auto"*/}
+      {/*  }}>*/}
+      {/*  <ImageCompareSlider image={REPRESENTATIVE_IMAGE} />*/}
+      {/*</div>*/}
+      <FileUploader />
+    </ContainerStyled>
+  );
 };
